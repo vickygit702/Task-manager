8f6f7d7884f26f688eac3563954411349e41ba04
vicky <vigneshhari702@gmail.com>
2025-07-12 21:49:46 +0530

    code app.py updated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.secret_key = '12345'
@@ -97,4 +98,5 @@ def delete(task_id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT env variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
